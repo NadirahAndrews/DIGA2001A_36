0928fe88d592f3d94127ebbed5a6131fcb1c0d9d
NadirahAndrews <nadirah.andrews@gmail.com>
2025-08-18 03:20:56 +0200

    script updated
    script updated

--- Happy-Birthday-Game/Assets/Target.cs
+++ Happy-Birthday-Game/Assets/Target.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public float strength = 10f;
-    public void Swing()
+    public void Swing(float value)
+    {
+        strength -= value;
+    }
+
 }
