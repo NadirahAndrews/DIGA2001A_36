534a10d62c42a1bbd8ad735eed8a8af91e01068b
NadirahAndrews <nadirah.andrews@gmail.com>
2025-08-18 03:36:06 +0200

    door script complete
    door script completed

--- Happy-Birthday-Game/Assets/Target.cs
+++ Happy-Birthday-Game/Assets/Target.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+// Reference: https://youtu.be/gx1Drc5Qx5g?si=YS7qGVrlpjy8ggqz
 
 public class Door : MonoBehaviour
 {
@@ -6,6 +7,13 @@ public class Door : MonoBehaviour
     public void Swing(float value)
     {
         strength -= value;
+        if (strength <= 0)
+        {
+            Close();
+        }
     }
-
+    void Close()
+    {
+        Destroy(gameObject);
+    }
 }
